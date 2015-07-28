a292091df48dd7fb4c589211cf77cd3521ee86b2
donatellosantoro <santoro.donatello@gmail.com>
2015-07-28 13:36:06 +0200

    Added expected test for employees

--- lunaticEngine/test/it/unibas/lunatic/test/mc/mainmemory/TestEmployeesNoSymmetric.java
+++ lunaticEngine/test/it/unibas/lunatic/test/mc/mainmemory/TestEmployeesNoSymmetric.java
@@ -3,26 +3,24 @@ package it.unibas.lunatic.test.mc.mainmemory;
 import it.unibas.lunatic.Scenario;
 import it.unibas.lunatic.model.chase.chasemc.ChaseMCScenario;
 import it.unibas.lunatic.model.chase.chasemc.DeltaChaseStep;
-import it.unibas.lunatic.model.dependency.Dependency;
 import it.unibas.lunatic.test.References;
 import it.unibas.lunatic.test.UtilityTest;
-import it.unibas.lunatic.test.checker.CheckTest;
-import junit.framework.Assert;
+import it.unibas.lunatic.test.checker.CheckExpectedSolutionsTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestEmployeesNoSymmetric extends CheckTest {
+public class TestEmployeesNoSymmetric extends CheckExpectedSolutionsTest {
 
     private static Logger logger = LoggerFactory.getLogger(TestEmployeesNoSymmetric.class);
 
     public void testScenario() throws Exception {
         Scenario scenario = UtilityTest.loadScenarioFromResources(References.employees_comparisons_nosymmetric);
-//        Scenario scenario = UtilityTest.loadScenarioFromResources(References.employees_comparisons_nosymmetric_dbms);
         setConfigurationForTest(scenario);
         scenario.getConfiguration().setUseSymmetricOptimization(false);
-//        scenario.getConfiguration().setUseLimit1(true);
         ChaseMCScenario chaser = scenario.getCostManager().getChaser(scenario);
         DeltaChaseStep result = chaser.doChase(scenario);
         if (logger.isDebugEnabled()) logger.debug(result.toStringLeavesOnlyWithSort());
+//        exportResults("/Temp/expectedEmployeesNoSymmetric", result);
+        checkExpectedSolutions("expectedEmployeesNoSymmetric", result);
     }
 }
