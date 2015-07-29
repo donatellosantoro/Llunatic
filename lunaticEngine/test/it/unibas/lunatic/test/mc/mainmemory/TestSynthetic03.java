1c36a493b35f872bca50995e3ac8ae8e50894274
Enzo Veltri <enzo.veltri@gmail.com>
2015-07-29 10:33:11 +0200

    Test for Synthetic 04-05-06-07

--- lunaticEngine/test/it/unibas/lunatic/test/mc/mainmemory/TestSynthetic03.java
+++ lunaticEngine/test/it/unibas/lunatic/test/mc/mainmemory/TestSynthetic03.java
@@ -1,16 +1,11 @@
 package it.unibas.lunatic.test.mc.mainmemory;
 
-import it.unibas.lunatic.LunaticConstants;
 import it.unibas.lunatic.Scenario;
 import it.unibas.lunatic.model.chase.chasemc.ChaseMCScenario;
 import it.unibas.lunatic.model.chase.chasemc.DeltaChaseStep;
 import it.unibas.lunatic.test.References;
 import it.unibas.lunatic.test.UtilityTest;
 import it.unibas.lunatic.test.checker.CheckExpectedSolutionsTest;
-import it.unibas.lunatic.test.comparator.repairs.PrecisionAndRecall;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import junit.framework.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,8 +21,8 @@ public class TestSynthetic03 extends CheckExpectedSolutionsTest {
 //        scenario.getConfiguration().setRemoveDuplicates(true);
         ChaseMCScenario chaser = scenario.getCostManager().getChaser(scenario);
         DeltaChaseStep result = chaser.doChase(scenario);
-        if (logger.isDebugEnabled()) logger.debug("DeltaDB: " + result.getDeltaDB().printInstances());
-        if (logger.isDebugEnabled()) logger.debug(result.toLongStringWithSort());
+//        if (logger.isDebugEnabled()) logger.debug("DeltaDB: " + result.getDeltaDB().printInstances());
+        if (logger.isDebugEnabled()) logger.debug(result.toStringWithSort());
         if (logger.isDebugEnabled()) logger.debug("Solutions: " + resultSizer.getPotentialSolutions(result));
         if (logger.isDebugEnabled()) logger.debug("Duplicate solutions: " + resultSizer.getDuplicates(result));
         Assert.assertEquals(6, resultSizer.getSolutions(result));
