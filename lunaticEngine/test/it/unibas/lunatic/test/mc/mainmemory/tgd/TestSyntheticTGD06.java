e8d8bdb2a5ba460321abc1cf6612b69cb5ec8fb2
Enzo Veltri <enzo.veltri@gmail.com>
2015-07-29 16:10:26 +0200

    Add expected for SyntheticTGD06

--- lunaticEngine/test/it/unibas/lunatic/test/mc/mainmemory/tgd/TestSyntheticTGD06.java
+++ lunaticEngine/test/it/unibas/lunatic/test/mc/mainmemory/tgd/TestSyntheticTGD06.java
@@ -6,11 +6,13 @@ import it.unibas.lunatic.model.chase.chasemc.DeltaChaseStep;
 import it.unibas.lunatic.model.dependency.Dependency;
 import it.unibas.lunatic.test.References;
 import it.unibas.lunatic.test.UtilityTest;
+import it.unibas.lunatic.test.checker.CheckExpectedSolutionsTest;
 import it.unibas.lunatic.test.checker.CheckTest;
+import junit.framework.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestSyntheticTGD06 extends CheckTest {
+public class TestSyntheticTGD06 extends CheckExpectedSolutionsTest {
 
     private static Logger logger = LoggerFactory.getLogger(TestSyntheticTGD06.class);
 
@@ -20,13 +22,19 @@ public class TestSyntheticTGD06 extends CheckTest {
 //        scenario.getConfiguration().setRemoveDuplicates(false);
         ChaseMCScenario chaser = scenario.getCostManager().getChaser(scenario);
         DeltaChaseStep result = chaser.doChase(scenario);
+        if (logger.isDebugEnabled()) logger.debug(scenario.toString());
         if (logger.isDebugEnabled()) logger.debug(result.toStringWithSort());
         if (logger.isDebugEnabled()) logger.debug("Solutions: " + resultSizer.getSolutions(result));
-        for (Dependency dependency : scenario.getExtEGDs()) {
-            if (logger.isDebugEnabled()) logger.debug(dependency.toLongString());
-        }
+//        for (Dependency dependency : scenario.getExtEGDs()) {
+//            if (logger.isDebugEnabled()) logger.debug(dependency.toLongString());
+//        }
 //        if (logger.isDebugEnabled()) logger.debug("Duplicate solutions: " + resultSizer.getDuplicates(result));
 //        checkExpectedInstances((MainMemoryDB) result, scenario);
         checkSolutions(result);
+        Assert.assertEquals(4, resultSizer.getPotentialSolutions(result));
+        Assert.assertEquals(0, resultSizer.getDuplicates(result));
+//        exportResults("/Users/enzoveltri/Temp/lunatic_tmp/expectedSyntheticTGD06", result);
+        checkExpectedSolutions("expectedSyntheticTGD06", result);
+
     }
 }
