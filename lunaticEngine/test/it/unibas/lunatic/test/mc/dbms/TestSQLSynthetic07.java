7c4da597afed8a452261a785f0420d356ee145de
Enzo Veltri <enzo.veltri@gmail.com>
2015-07-29 12:19:47 +0200

    Added Test for SQL Synthetic07

--- lunaticEngine/test/it/unibas/lunatic/test/mc/dbms/TestSQLSynthetic07.java
+++ lunaticEngine/test/it/unibas/lunatic/test/mc/dbms/TestSQLSynthetic07.java
@@ -5,11 +5,12 @@ import it.unibas.lunatic.model.chase.chasemc.ChaseMCScenario;
 import it.unibas.lunatic.model.chase.chasemc.DeltaChaseStep;
 import it.unibas.lunatic.test.References;
 import it.unibas.lunatic.test.UtilityTest;
-import it.unibas.lunatic.test.checker.CheckTest;
+import it.unibas.lunatic.test.checker.CheckExpectedSolutionsTest;
+import junit.framework.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestSQLSynthetic07 extends CheckTest {
+public class TestSQLSynthetic07 extends CheckExpectedSolutionsTest {
 
     private static Logger logger = LoggerFactory.getLogger(TestSQLSynthetic07.class);
 
@@ -22,9 +23,11 @@ public class TestSQLSynthetic07 extends CheckTest {
         if (logger.isDebugEnabled()) logger.debug(result.toStringWithSort());
         if (logger.isDebugEnabled()) logger.debug("Solutions: " + resultSizer.getPotentialSolutions(result));
         if (logger.isDebugEnabled()) logger.debug("Duplicate solutions: " + resultSizer.getDuplicates(result));
-//        Assert.assertEquals(9, resultSizer.getSize(result));
-//        Assert.assertEquals(0, resultSizer.getDuplicates(result));
 //        checkExpectedInstances((MainMemoryDB) result, scenario);
+        Assert.assertEquals(1, resultSizer.getSolutions(result));
+        Assert.assertEquals(0, resultSizer.getDuplicates(result));
         checkSolutions(result);
+        checkExpectedSolutions("expectedSynthetic07", result);
+
     }
 }
