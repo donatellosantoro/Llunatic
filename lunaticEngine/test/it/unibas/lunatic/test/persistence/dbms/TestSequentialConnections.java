40b4915e3d1fbd6ab566a47095853c8b3a2a761e
donatellosantoro <santoro.donatello@gmail.com>
2015-09-23 21:44:31 +0200

    Test added

--- lunaticEngine/test/it/unibas/lunatic/test/persistence/dbms/TestSequentialConnections.java
+++ lunaticEngine/test/it/unibas/lunatic/test/persistence/dbms/TestSequentialConnections.java
@@ -3,12 +3,12 @@ package it.unibas.lunatic.test.persistence.dbms;
 import it.unibas.lunatic.Scenario;
 import it.unibas.lunatic.test.References;
 import it.unibas.lunatic.test.UtilityTest;
-import it.unibas.lunatic.test.checker.CheckTest;
+import junit.framework.TestCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import speedy.model.database.dbms.DBMSDB;
 
-public class TestSequentialConnections extends CheckTest {
+public class TestSequentialConnections extends TestCase {
 
     private static Logger logger = LoggerFactory.getLogger(TestSequentialConnections.class);
     private DBMSDB database;
