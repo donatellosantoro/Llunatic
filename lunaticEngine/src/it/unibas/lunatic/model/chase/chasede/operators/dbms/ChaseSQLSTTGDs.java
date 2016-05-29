886c0d632d70d6e078a29d32d50882320bff4c32
donatellosantoro <santoro.donatello@gmail.com>
2016-05-29 19:47:37 +0200

    Bug fix in ChaseSQLSTTGDs

--- lunaticEngine/src/it/unibas/lunatic/model/chase/chasede/operators/dbms/ChaseSQLSTTGDs.java
+++ lunaticEngine/src/it/unibas/lunatic/model/chase/chasede/operators/dbms/ChaseSQLSTTGDs.java
@@ -71,7 +71,7 @@ public class ChaseSQLSTTGDs implements IChaseSTTGDs {
 
     private void executeScript(String script, AccessConfiguration accessConfiguration) {
         try {
-            QueryManager.executeScript(script, accessConfiguration, true, true, true, false);
+            QueryManager.executeScript(script, accessConfiguration, true, true, true, true);
         } catch (DBMSException ex) {
             if (ex.getMessage().contains("ERROR: function bigint_skolem(text) does not exist")
                     || ex.getMessage().contains("ERROR: function double_skolem(text) does not exist")) {
